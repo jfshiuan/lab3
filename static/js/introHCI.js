380bbaa69c446f95aece9334db27cb15bf28ab49
Jason Shiuan <jfshiuan@ucsd.edu>
2016-01-20 17:15:24 -0800

    complete

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
  */
  function initializePage() {
  	$("#testjs").click(function(e) {
- 		$('.jumbotron h1').text("Javascript is connected");
+ 		$('.jumbotron h1').text("You have pushed my button.");
  		$("#testjs").text("blabla");
  		$(".jumbotron p").addClass("active");
  	});
@@ -27,13 +27,13 @@ $(document).ready(function() {
 
 
 function updateProject(e) {
-   var projectID = $('#project').val();
-   $(projectID).animate({
-      width: $('#width').val()
-   });
+ var projectID = $('#project').val();
+ $(projectID).animate({
+  width: $('#width').val()
+});
 
-   var newText = $('#description').val();
-   $(projectID + " .project-description").text(newText);
+ var newText = $('#description').val();
+ $(projectID + " .project-description").text(newText);
 }
 
 
@@ -41,18 +41,22 @@ function projectClick(e) {
   // Cancel the default action, which prevents the page from reloading
   e.preventDefault();
 
-    // In an event listener, $(this) is the leement that fired the event
+    // In an event listener, $(this) is the element that fired the event
     var projectTitle = $(this).find("p").text();
     var jumbotronHeader = $(".jumbotron h1");
     jumbotronHeader.text(projectTitle);
 
 
+    var image = $(this).find("img");
+
 
     var containingProject = $(this).closest(".project");
     var description = $(containingProject).find(".project-description");
+    var descriptionText = $(this).find("p").text();
     if (description.length == 0) {
-    	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    	$(containingProject).append("<div class='project-description'><p>"+descriptionText+"</p></div>");
     } else {
     	$(description).fadeToggle();
+      $(image).fadeToggle();
     }
-}
+  }
